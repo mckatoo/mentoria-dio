37d7753c3b07f0d35285451a3b14614a33bd1108
Milton Carlos Katoo <mckatoo@gmail.com>
2022-03-03 10:11:34 -0300

    Setup inicial

--- api/src/settings.ts
+++ api/src/settings.ts
@@ -1,7 +1,7 @@
 export const IS_DEVELOPMENT = true;
 
 export const RABBIT_HOST= 'amqp://localhost';
-export const RABBIT_USER='';
-export const RABBIT_PASSWORD='';
+export const RABBIT_USER='admin';
+export const RABBIT_PASSWORD='admin';
 export const RABBIT_PORT=5672;
 export const RABBIT_DSN = IS_DEVELOPMENT ? `${RABBIT_HOST}` : ``;
